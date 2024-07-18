d2cce7998aedaf4e05fee013255684916fe2a3d7
Thital <mrtroll152@gmail.com>
2024-07-18 09:28:45 +0200

    1

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    id = models.AutoField(primary_key=True)
+    listing_title = models.CharField(max_length=30)
+    listing_description = models.CharField(max_length=90)
+    starting_bid = models.IntegerField()
+    listing_image = models.URLField()
